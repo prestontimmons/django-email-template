a8cb5e08f637b68ecfa0b22ac12c0a2d3b2a2407
Preston Timmons <prestontimmons@gfa.org>
2013-04-20 20:20:19 +0000

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from setuptools import setup, find_packages
 
+DESCRIPTION = """
+Send emails based on a Django template
+
+See:
+
+https://github.com/prestontimmons/django-email-template
+"""
+
+
 setup(
     name="django-email-template",
     version="1.0",
-    long_description=__doc__,
+    description="Send emails based on a Django template",
+    long_description=DESCRIPTION,
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
+    install_requires=[],
 )
