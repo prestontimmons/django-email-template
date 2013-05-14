895a21b774b0a3a8e3cc1b837e15c13359f65553
Preston Timmons <prestontimmons@gfa.org>
2013-05-14 20:54:25 +0000

    Updated send handling.
    Only specified arguments are passed to the send_method.
    
    The django sample works now, with tests.

--- email_template/email.py
+++ email_template/email.py
@@ -1,26 +1,31 @@
 from functools import partial
 
 from django.conf import settings
-from django.core.mail import send_mail
+from django.core.mail import send_mail as send_django_mail
 from django.template import Context, RequestContext
 from django.template.loader import get_template
 
 from .util import render_node
 
 
-def send_mail(template_name, context_data, send_method, render_method,
-        request=None, from_email=None, **kwargs):
+def send_base(template_name, context_data, send_method, render_method,
+        request=None, from_email=None, send_method_args=None, **kwargs):
 
-    kwargs["from_email"] = from_email or settings.DEFAULT_FROM_EMAIL
+    send_method_args = send_method_args or {}
 
-    kwargs.update(get_message(
+    args = get_message(
         template_name=template_name,
         context_data=context_data,
         request=request,
         render_method=render_method,
-    ))
+    )
 
-    return send_method(**kwargs)
+    args["from_email"] = from_email or settings.DEFAULT_FROM_EMAIL
+    args.update(send_method_args)
+
+    return send_method(**args)
+
+send_mail = send_base
 
 
 def get_message(template_name, context_data, request, render_method):
@@ -33,7 +38,7 @@ def get_message(template_name, context_data, request, render_method):
     return render_method(template, c)
 
 
-def render_template(template, context):
+def render_django_fields(template, context):
     message = {}
     message["message"] = render_node(template, "text", context)
     message["subject"] = render_node(template, "subject", context)
@@ -47,7 +52,16 @@ def render_template(template, context):
     return message
 
 
-send = partial(send_mail,
-    send_method=send_mail,
-    render_method=render_template,
+def send_django_wrapper(**kwargs):
+    return send_django_mail(
+        subject=kwargs["subject"],
+        message=kwargs["message"],
+        from_email=kwargs["from_email"],
+        recipient_list=kwargs["recipient_list"],
+    )
+
+
+send_django = partial(send_base,
+    send_method=send_django_wrapper,
+    render_method=render_django_fields,
 )
--- test_email_template/tests.py
+++ test_email_template/tests.py
@@ -8,7 +8,11 @@ from django.test.utils import (
     restore_template_loaders,
 )
 
-from email_template.email import render_template, send_mail
+from email_template.email import (
+    render_django_fields,
+    send_base,
+    send_django,
+)
 
 
 EMAIL = Template("""
@@ -26,13 +30,13 @@ def send_method(**kwargs):
     return kwargs
 
 
-send = partial(send_mail,
+send = partial(send_base,
     send_method=send_method,
-    render_method=render_template,
+    render_method=render_django_fields,
 )
 
 
-class SendMailTest(TestCase):
+class SendBaseTest(TestCase):
 
     def setUp(self):
         templates = {
@@ -51,7 +55,9 @@ class SendMailTest(TestCase):
             request=None,
             from_email="x@y.com",
             send_method=send_method,
-            header=2,
+            send_method_args=dict(
+                header=2,
+            )
         )
 
         self.assertEqual(message["message"], "Hello from Y")
@@ -82,3 +88,26 @@ class SendMailTest(TestCase):
         self.assertEqual(message["message"], "")
         self.assertEqual(message["subject"], "")
         self.assertEqual(message["recipient_list"], ["x@z.com", "y@z.com"])
+
+
+class SendDjangoTest(TestCase):
+
+    def setUp(self):
+        templates = {
+            "email.html": EMAIL,
+        }
+        setup_test_template_loader(templates)
+
+    def tearDown(self):
+        restore_template_loaders()
+
+    def test_send_django(self):
+        message = send_django(
+            template_name="email.html",
+            context_data=dict(name="Y"),
+            request=None,
+            from_email="x@y.com",
+            header=2,
+        )
+
+        self.assertEqual(message, 1)
